8df0ddaebc42a5d0fd3ec499178d7f91070bdf68
Alexis-Campomanes <alexiscp.developer@gmail.com>
2022-09-28 16:07:32 -0500

    tarea13

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Tarea11 from './Components/Tarea11';
+import Tarea13 from './Components/Tarea13/Tarea13';
 
 function App() {
   return (
     <div className="App">
-      <Tarea11 />
+      <Tarea13 />
     </div>
   );
 }
